5e9388110c292bd578153831b72462ca29c0e48c
MurrayWeir <mj.weir@btinternet.com>
2026-02-26 22:14:23 +0000

    converted to function

--- findNearestNHS.py
+++ findNearestNHS.py
@@ -4,30 +4,38 @@ import numpy as np
 import haversine as hs
 from haversine import Unit
 
-inputPostCode = "B10 0AB"
+def findNearestNHS(inputPostCode, uk_postcodes_df, Nhs_df):
+    # Select input postcode row in uk postcode df
+    inputPostcode_df = uk_postcodes_df.loc[uk_postcodes_df.PostCode == inputPostCode]
 
-# import uk postcodes
-colNames = ["Nation", "PostCode", "City", "Country", "CountryAbreviation", "County", "Unnamed1", "District", "Unnamed2", "Lat", "Long", "Id"]
-dType= {"Unnamed1": str, "District":str, "Lat":float, "Long":float}
-uk_postcodes_df = pd.read_csv("GB_full.txt", delimiter="\t", names=colNames, dtype=dType)
+    # Calculate harversine distance from input postcode to all nhs locations
+    loc1=(inputPostcode_df.Lat.to_numpy()[0], inputPostcode_df.Long.to_numpy()[0])
+    loc2 = Nhs_df[["Lat", "Long"]].to_numpy()
 
-# import nhs locations
-Nhs_df = uk_postcodes_df.sample(20) # replace this with nhs locations database
+    distances=hs.haversine_vector(loc1,loc2,unit=Unit.KILOMETERS, comb=True)
+    Nhs_df["Distance2InputPostCode"] = distances
+    
+    # Sort by distance
+    Nhs_df = Nhs_df.sort_values("Distance2InputPostCode")
 
-# select input postcode row in uk postcode df
-inputPostcode_df = uk_postcodes_df.loc[uk_postcodes_df.PostCode == inputPostCode]
+    return Nhs_df
 
-# calculate harversine distance from input postcode to all nhs locations
-loc1=(inputPostcode_df.Lat.to_numpy()[0], inputPostcode_df.Long.to_numpy()[0])
-loc2 = Nhs_df[["Lat", "Long"]].to_numpy()
 
-distances=hs.haversine_vector(loc1,loc2,unit=Unit.KILOMETERS, comb=True)
-Nhs_df["Distance2InputPostCode"] = distances
+if __name__ == "__main__":
 
-# sort by distance
-Nhs_df = Nhs_df.sort_values("Distance2InputPostCode")
+    inputPostCode = "B10 0AB"
 
-print(Nhs_df)
+    # Import uk postcodes
+    colNames = ["Nation", "PostCode", "Place", "Country", "CountryAbreviation", "County", "Unnamed1", "District", "Unnamed2", "Lat", "Long", "Id"]
+    dType= {"Unnamed1": str, "District":str, "Lat":float, "Long":float}
+    uk_postcodes_df = pd.read_csv("GB_full.txt", delimiter="\t", names=colNames, dtype=dType)
+
+    # Import nhs locations
+    Nhs_df = uk_postcodes_df.sample(20) # Replace this with nhs locations database
+
+    Nhs_df = findNearestNHS(inputPostCode, uk_postcodes_df, Nhs_df)
+
+    print(Nhs_df)
 
 
 
